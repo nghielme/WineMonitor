492a22d749b69839923c54bfdec8f8601236d53f
Nicolò Ghielmetti <nicolo.ghielmetti@gmail.com>
2020-05-13 01:51:24 +0200

    FIXED MQTT_MAX_PACKET_SIZE packet size problem. From 128 to 256. Now seems to work!

--- WineMonitor-ESP8266/mqtt_esp8266/wm_esp8266.ino.ino
+++ WineMonitor-ESP8266/mqtt_esp8266/wm_esp8266.ino.ino
@@ -1,26 +1,21 @@
 /*
  Basic ESP8266 MQTT example
-
  This sketch demonstrates the capabilities of the pubsub library in combination
  with the ESP8266 board/library.
-
  It connects to an MQTT server then:
   - publishes "hello world" to the topic "outTopic" every two seconds
   - subscribes to the topic "inTopic", printing out any messages
     it receives. NB - it assumes the received payloads are strings not binary
   - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
     else switch it off
-
  It will reconnect to the server if the connection is lost using a blocking
  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
  achieve the same result without blocking the main loop.
-
  To install the ESP8266 board, (using Arduino 1.6.4+):
   - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
        http://arduino.esp8266.com/stable/package_esp8266com_index.json
   - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
   - Select your ESP8266 in "Tools -> Board"
-
 */
 
 #define ARDUINOJSON_USE_DOUBLE 0
@@ -43,9 +38,9 @@
 //#define DHTTYPE DHT11   // DHT 11
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
-const char* ssid = "wi-fi ronchetto";
+const char* ssid = "WiFi_Ghielmetti-UP";
 const char* password = "Petauro*316";
-const char* mqtt_server = "winemonitoresp.ddns.net";
+const char* mqtt_server = "winemonitor.ddns.net";
 float tempIntNebbiolo = -1000;
 float tempIntCabernet = -1000;
 float tempExt = -1000;
@@ -63,7 +58,7 @@ String body;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 DHT dht(DHTPIN, DHTTYPE);
-AsyncDelay twoSecDelay, tenSecDelay;
+AsyncDelay fiveSecDelay;
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -96,25 +91,6 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
-}
 
 void reconnect() {
   // Loop until we're reconnected
@@ -123,10 +99,6 @@ void reconnect() {
     // Attempt to connect
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("intro", "connected");
-      // ... and resubscribe
-      client.subscribe("led");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -152,9 +124,10 @@ void sendDataMQTT(const bool& mustSave){
   root.printTo(body);
   body.toCharArray(msg,150);
   body = "";
-  Serial.print("Publish message: ");
-  Serial.println(msg);
-  client.publish("data", msg , body.length());
+  if(client.publish("data", msg)==true){
+    Serial.print("Published data: ");
+    Serial.println(msg);
+  }
   tempIntNebbiolo = root["tempIntNebbiolo"];
   tempIntCabernet = root["tempIntCabernet"];
   tempExt = root["tempExt"];
@@ -166,7 +139,6 @@ void setup() {
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
   timeClient.begin();
   timeClient.setUpdateInterval(3600);
   timeClient.update();
@@ -177,7 +149,7 @@ void setup() {
   if (!sensors.getAddress(outsideThermometer, 1)) Serial.println("Unable to find address for Device 1"); 
   sensors.setResolution(insideThermometer, TEMPERATURE_PRECISION);
   sensors.setResolution(outsideThermometer, TEMPERATURE_PRECISION);
-  twoSecDelay.start(2000, AsyncDelay::MILLIS);
+  fiveSecDelay.start(5000, AsyncDelay::MILLIS);
   timeClient.update();
 }
 
@@ -187,10 +159,10 @@ void loop() {
     reconnect();
   }
   client.loop();
-  if(!twoSecDelay.isExpired()) return;
-  twoSecDelay.repeat();
+  if(!fiveSecDelay.isExpired()) return;
+  fiveSecDelay.repeat();
   updateData();
-  if(isnan(root["tempIntNebbiolo"]) || isnan(root["tempIntCabernet"]) || isnan(root["tempExt"]) || isnan(root["humidity"])) return;
+  if(isnan((double)root["tempIntNebbiolo"]) || isnan((double)root["tempIntCabernet"]) || isnan((double)root["tempExt"]) || isnan((double)root["humidity"])) return;
   if(root["tempIntNebbiolo"] != tempIntNebbiolo || root["tempIntCabernet"] != tempIntCabernet || root["tempExt"] != tempExt || root["humidity"] != humidity)
     sendDataMQTT(true);
   else
